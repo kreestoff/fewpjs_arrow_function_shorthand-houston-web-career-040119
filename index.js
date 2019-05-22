b054bb3ad0802f025f93255659485cb4fe67b57e
kreestoff <christopher.l.mcintosh@outlook.com>
2019-05-21 22:03:28 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+let divide = (param1, param2) => {
+  return 2000 / 100
+}
+
+let square = (param1) => {
+  return param1 * param1
+}
+
+let add = (param1, param2) => {
+  return param1 + param2
+}
